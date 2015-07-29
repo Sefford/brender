02f0237c28d8a4ad07ea4d4cf28ca6dd94965058
Alejandro Rodriguez Salamanca <alexrs95@gmail.com>
2015-07-29 10:37:55 +0200

    Fix bug when using RecyclerFilterableAdapterData with headers or footers

--- src/main/java/com/sefford/brender/data/RecyclerFilterableAdapterData.java
+++ src/main/java/com/sefford/brender/data/RecyclerFilterableAdapterData.java
@@ -20,6 +20,7 @@ import android.widget.Filter;
 import com.sefford.brender.interfaces.RecyclerAdapterData;
 import com.sefford.brender.interfaces.Renderable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,10 +31,21 @@ import java.util.List;
 public abstract class RecyclerFilterableAdapterData extends Filter implements RecyclerAdapterData {
 
 
+
     /**
      * Master data of the adapter
      */
     protected final List<Renderable> master;
+
+    /**
+     * List containing headers. Headers will be always on top of the list
+     */
+    protected List<Renderable> headers = new ArrayList<>();
+
+    /**
+     * List containing footers. Footers will be always at the end of the list.
+     */
+    protected List<Renderable> footers = new ArrayList<>();
 
     /**
      * Filtered data of the adapter
@@ -57,12 +69,15 @@ public abstract class RecyclerFilterableAdapterData extends Filter implements Re
 
     @Override
     public long getItemId(int pos) {
-        return master.get(pos).getRenderableId();
-    }
-
-    @Override
-    public Renderable getItem(int pos) {
-        return filtered.get(pos);
+        if (pos < headers.size()){
+            return headers.get(pos).getRenderableId();
+        }else if (pos >= headers.size() && pos < master.size()){
+            pos -= headers.size();
+            return master.get(pos).getRenderableId();
+        }else {
+            pos -= (master.size() + headers.size());
+            return footers.get(pos).getRenderableId();
+        }
     }
 
     @Override
@@ -75,24 +90,47 @@ public abstract class RecyclerFilterableAdapterData extends Filter implements Re
         return this;
     }
 
+    @Override
+    public Renderable getItem(int pos) {
+        if (pos < headers.size()){
+            return headers.get(pos);
+        }else if (pos >= headers.size() && pos < filtered.size()){
+            pos -= headers.size();
+            return filtered.get(pos);
+        }else {
+            pos -= (filtered.size() + headers.size());
+            return footers.get(pos);
+        }
+    }
 
     @Override
     public void addFooter(Renderable data) {
-
+        footers.add(data);
     }
 
     @Override
     public void addHeader(Renderable data) {
-
+        headers.add(data);
     }
 
     @Override
     public void removeFooter(Renderable footer) {
-
+        footers.remove(footer);
     }
 
     @Override
     public void removeHeader(Renderable header) {
-
+        headers.remove(header);
     }
+
+    @Override
+    public int getHeaderViewCount() {
+        return headers.size();
+    }
+
+    @Override
+    public int getFooterViewCount() {
+        return footers.size();
+    }
+
 }
